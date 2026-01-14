e762dd4ba0183e23939c95f829e239ed4447eca0
HadiSam23 <hadi@365scores.com>
2026-01-14 17:17:50 +0200

    WIP

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,11 @@
 """Utility functions for the podcast generation service."""
 
 from .retry import with_retry
-from .date_helpers import format_game_time, parse_iso_datetime
+from .date_helpers import format_game_time, parse_iso_datetime, is_within_hours
 
 __all__ = [
     "with_retry",
     "format_game_time",
     "parse_iso_datetime",
+    "is_within_hours",
 ]
